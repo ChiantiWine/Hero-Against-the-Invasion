f15e2817138a5c7fb9c41cba2097bfcd06a44f6b
ChiantiWine <hsm11256@naver.com>
2025-01-19 00:59:50 +0900

    2025.01.19
    New Input System

--- Assets/Scripts/Player Controls.cs
+++ Assets/Scripts/Player Controls.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputAction movement;
+     void Start()
     {
         
+    }
+    // 유니티의 실행 순서 Awake -> OnEnable
+    void OnEnable() 
+    {
+        movement.Enable();    
+    }
+
+    // 유니티 실행 순서
+    void OnDisable() 
+    {
+        movement.Disable();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // float verticalThrow = Input.GetAxis("Vertical");
     }
 }
