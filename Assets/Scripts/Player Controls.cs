5e0729d5882ca79a7f7ac82325ff22d4fc726f48
ChiantiWine <hsm11256@naver.com>
2025-01-22 23:37:41 +0900

    보완 필요
    1. clamp 수정 필요
    2. skybox 수정
    3. 장면 중 Laser 수정필요

--- Assets/Scripts/Player Controls.cs
+++ Assets/Scripts/Player Controls.cs
@@ -1,9 +1,22 @@
+using Mono.Cecil;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
+   [SerializeField] InputAction movement;
+   [SerializeField] InputAction fire;
+   [SerializeField] GameObject[] lasers;
+    [SerializeField] float controlSpeed = 30f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 10f;
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float positionYawFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float controlRollFactor = -15f;
+    
+    float xThrow, yThrow;
      void Start()
     {
         
@@ -11,21 +24,93 @@ public class PlayerControls : MonoBehaviour
     // 유니티의 실행 순서 Awake -> OnEnable
     void OnEnable() 
     {
-        movement.Enable();    
+        movement.Enable();
+        fire.Enable();   
     }
 
     // 유니티 실행 순서
     void OnDisable() 
     {
-        movement.Disable();    
+       movement.Disable();
+       fire.Disable();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-        // float horizontalThrow = Input.GetAxis("Horizontal");
-        // float verticalThrow = Input.GetAxis("Vertical");
+      
+        ProcessTranslation();
+        ProcessRotation();
+        ProcessFiring();
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+    void ProcessTranslation()
+    {
+        // Old
+        // xThrow = Input.GetAxis("Horizontal");
+        // yThrow = Input.GetAxis("Vertical");
+
+        // new Input System
+        xThrow = movement.ReadValue<Vector2>().x;
+        yThrow = movement.ReadValue<Vector2>().y;
+
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        // Player Rig에서 Position을 움직이게 되면 World 좌표 이동(카메라가 이동됨) -> 원하는 건  Local에서 Position 이동 -> Player Ship에 Script 컴퍼트
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessFiring()
+    {
+        if(fire.ReadValue<float>() > 0.5)
+        {
+            ActiveLasers();
+        }
+        else
+        {
+            DeactiveLasers();
+        }
+        //  if(Input.GetButton("Fire1"))
+        //  {
+        //
+        //  }
+        //  else
+        //  {
+        //  
+        //  }
+    }
+
+    private void ActiveLasers()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(true);
+        }
+    }
+
+    void DeactiveLasers()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(false);
+        }
     }
 }
