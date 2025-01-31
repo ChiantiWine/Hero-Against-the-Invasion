0cfe01cb9a074b7adcabae826b05ddc4cf4b6221
ChiantiWine <hsm11256@naver.com>
2025-01-31 23:14:42 +0900

    2025.01.31
    1. 적 wave 및 TimeLine 설정

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,16 +2,53 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using Unity.Mathematics;
+using UnityEditor.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
-    void OnParticleCollision(GameObject other) 
+    [SerializeField] GameObject hitVFX;
+    
+    [SerializeField] int scorePerHit = 1;
+    [SerializeField] int hitPoint = 4;
+    ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
+    void Start()
+    {
+        scoreBoard = FindAnyObjectByType<ScoreBoard>();
+        parentGameObject = GameObject.FindGameObjectWithTag("SpawnAtRuntime");
+        AddRigidbody();
+    }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if(hitPoint < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        hitPoint--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
     {
         // vfx 게임 오브젝트로 저장(인스턴스화)
         GameObject vfx = Instantiate(deathVFX, transform.position, quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
+
+  
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+   int score;
+   TMP_Text scoreText;
+
+    void Start() 
+    {
+        // scoreText.GetComponent<TMP_Text>();
+        // if(scoreText != null)
+        // scoreText.text = "Start";
+        // Debug.Log($"{scoreText} ------");
+    }
+    
+   public void IncreaseScore(int amountToIncrease)
+   {
+        score += amountToIncrease;
+        // if(scoreText != null)
+       // scoreText.text = score.ToString();
+   }
+}
